c99209633f9b5a8df88946d8495d171f30f81488
Vishal <vishal.mgvk@gmail.com>
2020-04-11 19:56:46 -0400

    Updatde a file

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const NavBar = ({totalCounters}) => {
     return (<nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="/">
-            Navbar{" "}
+            Number of Products selected from the list : {" "}
             <span className="badge badge-pill badge-secondary"> {totalCounters} </span>
         </a>
     </nav>);
